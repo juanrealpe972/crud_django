4cb8e239e9d8dcca3468da725d26e4da2c3d04ab
juanrealpe972 <juanrealpe972@gmail.com>
2024-06-29 22:14:35 -0500

    Inicializando repositorio y agregando el registro de usuario en una base de datos sqlite

---
+++ tasks/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, 'signup.html', {
+            'form' : UserCreationForm
+        })
+    else: 
+        if request.POST['password1'] == request.POST['password2']:
+            # Register user
+            try:
+                user = User.objects.create_user(username= request.POST['username'], password=request.POST['password2'])
+                user.save()
+                return redirect('tasks')
+            except:
+                return render(request, 'signup.html', {
+                    'form' : UserCreationForm,
+                    'error': 'Username already exists'
+                })
+        else:
+            return render(request, 'signup.html', {
+                'form' : UserCreationForm,
+                'error': 'Password do not match'
+            })
+        
+def tasks(request) : 
+    return render(request, 'tasks.html')
+
